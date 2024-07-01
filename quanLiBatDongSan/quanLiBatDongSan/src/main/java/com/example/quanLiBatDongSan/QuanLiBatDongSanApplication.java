856a34ffc76544291fc110d5180e27d3b82ff56b
fuochung0905 <6251071033@st.utc2.edu.vn>
2024-07-01 15:14:50 +0700

    start

---
+++ quanLiBatDongSan/quanLiBatDongSan/src/main/java/com/example/quanLiBatDongSan/QuanLiBatDongSanApplication.java
@@ -0,0 +1,13 @@
+package com.example.quanLiBatDongSan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLiBatDongSanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanLiBatDongSanApplication.class, args);
+	}
+
+}
